9e29564896c8d343bc59676e222132db70e490d1
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-01 05:40:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,14 +16,13 @@ class Artist
   end
   
   def add_song(song)
-    #@@song_count += 1
+    @@song_count += 1
     @songs << song
     song.artist = self
   end
   
   def add_song_by_name(name)
-   # @@song_count += 1
-  @songs << song.name
+    @@song_count += 1
     song = Song.new(name)
     add_song(song)
     self << song
