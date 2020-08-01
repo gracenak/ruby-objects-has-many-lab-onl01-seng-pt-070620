8b83cc7191244d74400d7c4b43d3fd7247f96a4f
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-01 04:21:28 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+  
+  attr_accessor :title
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author
+    self.new
+  end
+  
+  def author_name
+    self.author.name
+  end
+  
+  
+end
