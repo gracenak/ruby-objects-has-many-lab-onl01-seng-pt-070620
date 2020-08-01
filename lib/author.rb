3625fdb1982cf8026ee4a28b77c9137bbf87153b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-08-01 04:28:48 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,25 @@
 class Author
   
-  attr_accessor :name
+  attr_accessor :name, :posts
   
   def initialize(name)
     @name = name
+    @posts = posts
   end
   
   def posts
     @posts
   end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = post.new(title)
+    add_post(post)
+  end
+    
   
   
 end
